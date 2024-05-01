727d5225720f2a485737bf0bd7c191b3bc3b3fa5
Dawitt-dev <davoza27@yahoo.com>
2024-05-01 14:41:54 -0700

    task_4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,23 @@
 """module for the api"""
 
 from api.v1.views import app_views
-from flask import jsonify
+from flask import Flask, jsonify
 from models import storage
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """returns a json response"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ Return a JSON stats response """
+    classes = {"Amenity": "amenities", "City": "cities",
+               "Place": "places", "Review": "reviews",
+               "State": "states", "User": "users"}
+    stats = {}
+    for key, value in classes.items():
+        stats[value] = storage.count(key)
+    return jsonify(stats)

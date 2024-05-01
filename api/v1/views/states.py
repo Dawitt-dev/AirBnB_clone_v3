c275ab99e2d3fc39581cf9d7397fe5627ec118db
Dawitt-dev <davoza27@yahoo.com>
2024-05-01 15:43:45 -0700

    task_6

---
+++ api/v1/views/states.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""new view for State objects that handles all default RESTFul API actions"""
+
+from flask import Flask, jsonify, abort, request
+from models import storage
+from models.state import State
+from api.v1.views import app_views
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def retrieve_states():
+    """retrieves all State objects"""
+    states = storage.all(State)
+    state_list = []
+    for state in states.values():
+        state_list.append(state.to_dict())
+    return jsonify(state_list)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def retrieve_state(state_id):
+    """retrieves a State object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def remove_state(state_id):
+    """deletes a State object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """creates a State object"""
+    if not request.json:
+        abort(400, 'Not a JSON')
+    if 'name' not in request.json:
+        abort(400, 'Missing name')
+    state = State(**request.json)
+    state.save()
+    return jsonify(state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """updates a State object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    if not request.json:
+        abort(400, 'Not a JSON')
+    for key, value in request.json.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict())
+
+
+if __name__ == "__main__":
+    pass
